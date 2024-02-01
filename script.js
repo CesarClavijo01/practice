ccf7c2dea4f618b2c462d8fd51b315f6c3afb843
Cesar <cesarclavijo778@gmail.com>
2024-01-31 21:37:11 -0600

    practice

---
+++ script.js
@@ -0,0 +1,19 @@
+// Grab a toothbrush with your hand.
+// Turn on the water.
+// Rinse the brush part of the toothbrush with water.
+//Turn off the water.
+// Open the tube of toothpaste.
+// Squeeze toothpaste on the brush part of the toothbrush.
+// Start with the front teeth.
+// Move in circular motions with the brush.
+let numCows = 50;
+const x = "Hello World"
+console.log("Hello " + "Disney Land!")
+console.log(x)
+var num = 5+1
+console.log(num)
+const str = 'Hello' + ' ' + 'World'
+let str1 = "Hello,";
+let str2 = "I am";
+const name = "Josh";
+alert(str1 + " " + str2 + " " + name)
